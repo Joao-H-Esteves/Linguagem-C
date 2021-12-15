986ac7d8043cbcb00f9b803dfbd74d8aabb94d6b
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2021-12-15 15:14:49 -0300

    structs

---
+++ Cadastro de cliente com struct/Cadastro de cliente com struct/Cadastro de cliente com struct.cpp
@@ -0,0 +1,12 @@
+// Cadastro de cliente com struct.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
--- Struct - ESTRUTURAS/Struct - ESTRUTURAS/Struct - ESTRUTURAS.cpp
+++ Struct - ESTRUTURAS/Struct - ESTRUTURAS/Struct - ESTRUTURAS.cpp
@@ -1,32 +1,43 @@
-// Struct - ESTRUTURAS.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
+// Struct - ESTRUTURAS.cpp : COMO MANIPILAR STRUCT
+// DECLARAÇÃO E MANIPULAR
 
 #include <iostream>
 
 struct horario			// Definindo como será a Struct
 	{
-	char DiaDaSemana;
-	int horas;
-	int minutos;
-	int segundos;
-	float decimos;
+		char DiaDaSemana; // PARA CRIAR A STRUCT É NECESSARIO AS CHAVES { }
+		int horas;
+		int minutos;
+		int segundos;
+		float decimos;
 	};
 
-int main()
+int teste(void)//vazio
 {
-
-
-
-
-	struct horario atual { // Declarando a Struct (dando um nome para ela)
-		atual.DiaDaSemana = 'Q';
+	
+	struct horario atual;  // Declarando a Struct (dando um nome para ela)
+		atual.DiaDaSemana = 'T';
 		atual.horas = 13;
 		atual.minutos = 34;
 		atual.segundos = 47;
 		atual.decimos = 0.483543589;
 		
-	};
 
+		// declarando uma nova Struct e utilizando a "atual" para somar suas variaveis.
+		struct horario posterior;
+		posterior.DiaDaSemana = 'Q';
+		posterior.horas = atual.horas + 5;
+		posterior.minutos = atual.minutos + 35;
+		posterior.segundos = atual.segundos - 22;
+		posterior.decimos = atual.decimos + 0.3242345;
+
+
+		// MOSTRANDO OS DADOS DA STRUCT NOVA
+
+		printf("Dia da semana = %c \n ", posterior.DiaDaSemana);
+		printf("Horario = %d : %d : %d : %.5f\n", posterior.horas, posterior.minutos, posterior.segundos, posterior.decimos);
+
+	system("pause");
 
  return 0;
 
