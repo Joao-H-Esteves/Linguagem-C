44ba91c9ced74e822a4f6bd93670915288abfd51
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2021-12-21 18:17:06 -0300

    NOVO CONTEUDO

---
+++ Exercicios/Função com Struct/Função com Struct/Função com Struct.cpp
@@ -0,0 +1,9 @@
+// Função com Struct.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//https://www.youtube.com/watch?v=jsMxEWCGpmk
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
