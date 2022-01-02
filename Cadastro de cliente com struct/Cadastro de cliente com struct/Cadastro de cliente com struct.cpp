9510a45fa17876d837e9893543a0ffa0d9766782
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-01-02 13:25:59 -0300

    codigo completo
    revisado e sem bugs

--- Cadastro de cliente com struct/Cadastro de cliente com struct/Cadastro de cliente com struct.cpp
+++ Cadastro de cliente com struct/Cadastro de cliente com struct/Cadastro de cliente com struct.cpp
@@ -16,7 +16,7 @@ struct tp_endereco {
     char cidade[20];
     char estado[10];
     int cep;
-}   end;        // Nome que usaremos paRA CHAMAR A ESTRUTURA NO CODIGO
+}   end;        // Nome que usaremos para CHAMAR A ESTRUTURA NO CODIGO
 
 struct tp_nascimento {
     int dia;
@@ -48,59 +48,72 @@ int main(void)
 
         printf("\n\nNome do cliente.......:");
         memset(cad_principal[i].nome, 0, strlen(cad_principal[i].nome));
-        scanf_s(" %40s%*c", cad_principal[i].nome, 40);
+        gets_s(cad_principal[i].nome);
+        //scanf_s(" %40s%*c", cad_principal[i].nome, 40); erro ao dar espaço
 
-
+        fflush(stdin);      //Limpar Buffer
 
         printf("\n\nCPF...................:");
         memset(cad_principal[i].cpf, 0, strlen(cad_principal[i].cpf));
-        scanf_s("%11s%*c", &cad_principal[i].cpf);
+        gets_s(cad_principal[i].cpf);
+        //scanf_s("%11s%*c", &cad_principal[i].cpf,11);
 
-
+        fflush(stdin);
 
         printf("\n\nTelefone..............:");
         scanf_s(" %d%*c", &cad_principal[i].tel);
 
-
+        fflush(stdin);
 
         printf("\n\nEndereço..............:");
         memset(cad_principal[i].end.rua, 0, strlen(cad_principal[i].end.rua));
-        scanf_s(" %40s%*c", cad_principal[i].end.rua, 40);
+        gets_s(cad_principal[i].end.rua);
+        //scanf_s(" %40s%*c", cad_principal[i].end.rua, 40);
 
-
+        fflush(stdin);
 
         printf("\n\nNumero................:");
         scanf_s(" %d%*c", &cad_principal[i].end.num);
 
+        fflush(stdin);
 
         printf("\n\nBairro................:");
         memset(cad_principal[i].end.bairro, 0, strlen(cad_principal[i].end.bairro));
-        scanf_s(" %20s%*c", cad_principal[i].end.bairro, 20);
+        gets_s(cad_principal[i].end.bairro);
+        //scanf_s(" %20s%*c", cad_principal[i].end.bairro, 20);
 
+        fflush(stdin);
 
         printf("\n\nCidade................:");
         memset(cad_principal[i].end.cidade, 0, strlen(cad_principal[i].end.cidade));
-        scanf_s(" %20s%*c", cad_principal[i].end.cidade, 20);
+        gets_s(cad_principal[i].end.cidade);
+        //scanf_s(" %20s%*c", cad_principal[i].end.cidade, 20);
 
+        fflush(stdin);
 
-        printf("\n\nEstado................:");
+        printf("\n\nEstado(xx)............:");
         memset(cad_principal[i].end.estado, 0, strlen(cad_principal[i].end.estado));
-        scanf_s(" %10s%*c", cad_principal[i].end.estado, 10);
+        gets_s(cad_principal[i].end.estado);
+        //scanf_s(" %10s%*c", cad_principal[i].end.estado, 10);
 
+        fflush(stdin);
 
         printf("\n\nCEP...................:");
         scanf_s(" %d%*c", &cad_principal[i].end.cep);
 
+        fflush(stdin);
 
-        printf("\n\nDia de Nascimento.....:");
+        printf("\n\nDia de Nascimento(xx).:");
         scanf_s(" %d%*c", &cad_principal[i].nasc.dia);
 
+        fflush(stdin);
 
-        printf("\n\nMes de Nascimento.....:");
+        printf("\n\nMes de Nascimento(xx).:");
         scanf_s(" %d%*c", &cad_principal[i].nasc.mes);
 
+        fflush(stdin);
 
-        printf("\n\nAno de Nascimento.....:");
+        printf("\n\nAno de Nascimento(xxxx):");
         scanf_s(" %d%*c", &cad_principal[i].nasc.ano);
 
 
@@ -117,11 +130,11 @@ int main(void)
         printf("\nNome do cliente.......: %s", cad_principal[i].nome);
         printf("\nCPF...................: %s", cad_principal[i].cpf);
         printf("\nTelefone..............: %d", cad_principal[i].tel);
-        printf("\nEndereço..............: %s", cad_principal[i].end.rua);
+        printf("\nEndereco..............: %s", cad_principal[i].end.rua);
         printf("\nNumero................: %d", cad_principal[i].end.num);
         printf("\nBairro................: %s", cad_principal[i].end.bairro);
         printf("\nCidade................: %s", cad_principal[i].end.cidade);
-        printf("\nEstado.................: %s", cad_principal[i].end.estado);
+        printf("\nEstado................: %s", cad_principal[i].end.estado);
         printf("\nCEP...................: %d", cad_principal[i].end.cep);
         printf("\nDia de Nascimento.....: %d", cad_principal[i].nasc.dia);
         printf("\nMes de Nascimento.....: %d", cad_principal[i].nasc.mes);
